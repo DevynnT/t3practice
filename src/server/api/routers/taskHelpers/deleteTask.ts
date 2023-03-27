9c4a84d8616cc00f35539a6f40b39cf06a8ce3b8
Devynn <personaldevynn@gmail.com>
2023-03-27 11:07:52 -0700

    initial commit

---
+++ src/server/api/routers/taskHelpers/deleteTask.ts
@@ -0,0 +1,43 @@
+import { protectedProcedure } from "../../trpc";
+import { z } from "zod";
+
+export default protectedProcedure
+    .input(
+        z.object({
+            id: z.string(),
+        })
+    )
+    .mutation(async ({ ctx, input }) => {
+        const task = await ctx.prisma.task.findFirstOrThrow({
+            where: {
+                id: input.id,
+                userId: ctx.session.user.id,
+            },
+        });
+        
+        await ctx.prisma.task.delete({
+            where: {
+                id: input.id,
+            },
+        })
+    });
+
+// export default protectedProcedure
+//   .input(
+//     z.object({
+//       id: z.string(),
+//     })
+//   )
+//   .mutation(async ({ ctx, input }) => {
+//     await ctx.prisma.task.findFirstOrThrow({
+//       where: {
+//         id: input.id,
+//         userId: ctx.session.user.id,
+//       },
+//     });
+//     await ctx.prisma.task.delete({
+//       where: {
+//         id: input.id,
+//       },
+//     });
+//   });
